cd365bfbeb2934a22547a40d7f9af3391950e92d
Ekaterina <teos2910@mail.ru>
2023-10-05 16:23:25 +0500

    task07

---
+++ task07_Последняя_цифра/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+7. Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int result = numberA % 10;
+System.Console.WriteLine(result);
