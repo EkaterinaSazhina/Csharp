a66db37f3152e380e30d76212e7a4b3953e52f86
Ekaterina <teos2910@mail.ru>
2023-10-10 16:57:41 +0500

    task15

--- task03_Дни_недели/Program.cs
+++ task03_Дни_недели/Program.cs
@@ -5,19 +5,12 @@
 */
 Console.WriteLine("Введите номер дня недели: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA == 1) Console.WriteLine("Понедельник");
+if (numberA == 2) Console.WriteLine("Вторник");
 if (numberA == 3) Console.WriteLine("Среда");
-
+if (numberA == 4) Console.WriteLine("Четверг");
 if (numberA == 5) Console.WriteLine("Пятница");
-
-if (numberA == 1)Console.WriteLine("Понедельник");
-
-if (numberA == 2)Console.WriteLine("Вторник");
-
-if (numberA == 4)Console.WriteLine("Четверг");
-
-if (numberA == 6)Console.WriteLine("Суббота");
-
-if (numberA == 7)Console.WriteLine("Воскресенье");
-
-else System.Console.WriteLine("нет такого дня недели");
+if (numberA == 6) Console.WriteLine("Суббота");
+if (numberA == 7) Console.WriteLine("Воскресенье");
+else if (numberA > 7) System.Console.WriteLine("нет такого дня недели");
 
---
+++ task15_Выходной/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+ и проверяет, является ли этот день выходным.
+*/
+Console.WriteLine("Введите номер дня недели: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA == 1) Console.WriteLine("Понедельник-рабочий день");
+if (numberA == 2) Console.WriteLine("Вторник-рабочий день");
+if (numberA == 3) Console.WriteLine("Среда-рабочий день");
+if (numberA == 4) Console.WriteLine("Четверг-рабочий день");
+if (numberA == 5) Console.WriteLine("Пятница-рабочий день");
+if (numberA == 6) Console.WriteLine("Суббота!Ура выходной!");
+if (numberA == 7) Console.WriteLine("Воскресенье!Ура выходной!");
+else if (numberA > 7) System.Console.WriteLine("нет такого дня недели");
