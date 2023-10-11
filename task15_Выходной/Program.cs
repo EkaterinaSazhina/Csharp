37f79846e37c580d7bcc3918f61e01c044543b81
Ekaterina <teos2910@mail.ru>
2023-10-11 16:42:16 +0500

    task15

--- task15_Выходной/Program.cs
+++ task15_Выходной/Program.cs
@@ -4,11 +4,15 @@
 */
 Console.WriteLine("Введите номер дня недели: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-if (numberA == 1) Console.WriteLine("Понедельник-рабочий день");
-if (numberA == 2) Console.WriteLine("Вторник-рабочий день");
-if (numberA == 3) Console.WriteLine("Среда-рабочий день");
-if (numberA == 4) Console.WriteLine("Четверг-рабочий день");
-if (numberA == 5) Console.WriteLine("Пятница-рабочий день");
-if (numberA == 6) Console.WriteLine("Суббота!Ура выходной!");
-if (numberA == 7) Console.WriteLine("Воскресенье!Ура выходной!");
-else if (numberA > 7) System.Console.WriteLine("нет такого дня недели");
+if (numberA > 0 && numberA <= 5)
+{
+    System.Console.WriteLine("Рабочий день!");
+}
+else if (numberA == 6 || numberA == 7)
+{
+    System.Console.WriteLine("Ура!Выходной!");
+}
+else 
+{
+    System.Console.WriteLine("Нет такого дня недели");
+}
