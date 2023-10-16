8e39ec0b1150c30535331aed96d8bee2d3e772a2
Ekaterina <teos2910@mail.ru>
2023-10-16 14:40:40 +0500

    task13

---
+++ task24_Сумма_чисел/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+24. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+/*System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+for (int i = 1; i <= number; i++)
+    {
+     result = result + i;
+     
+    }
+
+System.Console.WriteLine(result);*/
+
+System.Console.Write("Введите число: ");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Summer(int number)
+{
+    int summ = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        summ = summ + i;
+    }
+    return summ;
+}
+
+int userResult = Summer(userA);
+System.Console.WriteLine(userResult);
---
+++ task28_Произведение_чисел/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+28. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/ 
+System.Console.Write("Введите число: ");
+int userA = Convert.ToInt32(Console.ReadLine());
+
+int Summer(int number)
+{
+    int summ = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        summ = summ * i;
+    }
+    return summ;
+}
+
+int userResult = Summer(userA);
+System.Console.WriteLine(userResult);
