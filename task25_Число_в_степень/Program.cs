e65946af3677d61b27460dcf875686cf7f693502
Ekaterina <teos2910@mail.ru>
2023-10-16 21:45:04 +0500

    task29

---
+++ task25_Число_в_степень/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/ 
+System.Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+double result = Math.Pow(numberA, numberB);
+System.Console.WriteLine(result);
---
+++ task27_Сумма_цифр/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Sum(int num)
+{
+    int root = 0;
+    int result = 0;
+    for (int i = number; i > 0; i = i / 10)
+    {
+        root = i % 10;
+        result = result + root;
+    }
+    return result;
+}
+int userResalt = Sum(number);
+System.Console.WriteLine(userResalt);
