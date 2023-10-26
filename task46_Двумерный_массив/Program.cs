b6eb515518d3d97c436ef5742815ca18275df5d8
Ekaterina <teos2910@mail.ru>
2023-10-26 13:46:07 +0500

    task51

---
+++ task46_Двумерный_массив/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+*/ 
+System.Console.Write("Укажите количество строк: ");
+int userM = int.Parse(System.Console.ReadLine());
+System.Console.Write("Укажите количество столбцов: ");
+int userN = int.Parse(System.Console.ReadLine());
+int[,] matrix = new int[userM, userN];
+void PrintArray(int[,] matr)
+{
+
+    for (int i = 0; i < userM; i++)
+    {
+        for (int j = 0; j < userN; j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FeelArray(int[,]matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] =  new Random().Next(1,10);
+        }
+    }
+}
+
+PrintArray(matrix);
+FeelArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
---
+++ task48_Массив_с_формулой/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задайте двумерный массив размера m на n,
+каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+Выведите полученный массив на экран.
+*/ 
+System.Console.Write("Укажите количество строк: ");
+int userM = int.Parse(System.Console.ReadLine());
+System.Console.Write("Укажите количество столбцов: ");
+int userN = int.Parse(System.Console.ReadLine());
+int[,] matrix = new int[userM, userN];
+void PrintArray(int[,] matr)
+{
+
+    for (int i = 0; i < userM; i++)
+    {
+        for (int j = 0; j < userN; j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FeelArray(int[,]matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] =  i+j;
+        }
+    }
+}
+
+PrintArray(matrix);
+FeelArray(matrix);
+System.Console.WriteLine();
+PrintArray(matrix);
